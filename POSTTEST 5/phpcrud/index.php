785d3d1ed451c9a386b72373c72cbad0db9844ed
angelita1005 <114124620+angelita1005@users.noreply.github.com>
2022-10-22 19:28:56 +0800

    Add files via upload

---
+++ POSTTEST 5/phpcrud/index.php
@@ -0,0 +1,14 @@
+<?php
+include 'functions.php';
+
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>HOME</h2>
+	<p>WELLCOME AT CRUD ANGEL</p>
+	<P> Anyenghaseo Yeorobun </P>
+</div>
+
+<?=template_footer()?>
